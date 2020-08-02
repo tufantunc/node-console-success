96bbd4acbbff2dd3c310f3f38ea60e8b2e0e356b
Tufan Tunç <tufan.tunc@nesine.com>
2020-08-02 15:49:24 +0300

    packages updated

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,6 +16,7 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config)
   on('file:preprocessor', require('@cypress/code-coverage/use-browserify-istanbul'))
+  return config
 }
